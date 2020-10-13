43e3f5766c18b5be66ef32e28ccb5e10174aeb69
Ladislav Suchý <suchy26@uniba.sk>
2020-10-13 22:26:07 +0200

    Add project files.

---
+++ Kryci-prispevok/Excel/ExcelManager.cs
@@ -0,0 +1,34 @@
+﻿
+
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Kryci_prispevok
+{
+    class ExcelManager
+    {
+        public void Spracuj_Excel(string ExcelName)
+        {
+            if (ExcelName == null)
+                return;
+            FileInfo Excel = new FileInfo(ExcelName);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var pckg = new ExcelPackage(Excel))
+            {
+                var exlwk = pckg.Workbook.Worksheets[0];
+                var ExcelText = exlwk.Cells["A1"].Value;
+
+                ExcelPackage ExcelPkg = new ExcelPackage();
+                ExcelWorksheet wsSheet1 = ExcelPkg.Workbook.Worksheets.Add("Sheet1");
+                wsSheet1.Cells["A1"].Value = ExcelText + "something";
+
+                wsSheet1.Protection.IsProtected = false;
+                wsSheet1.Protection.AllowSelectLockedCells = false;
+                ExcelPkg.SaveAs(new FileInfo("NewExcel/Excel.xlsx"));
+            }
+           
+        }
+    }
+}
---
+++ Kryci-prispevok/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kryci_prispevok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ExcelManager ExlMng = new ExcelManager();
+        private void Pick_File(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.xlsx)|*.xlsx";
+            if (openFileDialog.ShowDialog() == true)
+                ExlMng.Spracuj_Excel(openFileDialog.FileName);
+                
+        }
+    }
+}
